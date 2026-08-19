f1f2ae17d38a936f2a55328e42df5482bb99642b
Madhav <madhavraghavak47@gmail.com>
2026-08-19 22:52:34 +0530

    set1

---
+++ set1_q7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n){
+    if(n<=1)return 1 ;
+    else return fib(n-1) + fib(n-2);
+}
+
+int main(){ int x ;
+    cout<<"Enter nuber of digits " ;
+    cin>>x ;
+    cout<<"the series is ";
+    for (int i =1 ; i<=x;i++){
+        cout<<fib(i)<<" " ;
+    }
+    return 0;
+}
